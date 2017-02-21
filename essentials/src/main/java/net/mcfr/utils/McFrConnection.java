4115090db820a53e726791d76ba2aeca402e229a
Lepticed <cedtrabou@gmail.com>
2017-02-21 13:46:09 +0100

    refactor(McFrConnection): Utilisation d'un bloc statique.

--- essentials/src/main/java/net/mcfr/utils/McFrConnection.java
+++ essentials/src/main/java/net/mcfr/utils/McFrConnection.java
@@ -15,16 +15,35 @@ import com.google.gson.stream.JsonReader;
 
 public final class McFrConnection {
   private static String jdbcUrl;
-  private static boolean configRead = false;
   private static String jdrDatabase;
   private static String serverDatabase;
   private static SqlService sql = Sponge.getServiceManager().provide(SqlService.class).get();
 
-  public static Connection getJdrConnection() {
-    if (!configRead) {
-      readConfigFile();
-      configRead = true;
+  /**
+   * Lit les identifiants de connexion renseignées dans le fichier de config : config/essentials-config/database.json.
+   * 
+   * Fichier de la forme (valeurs par défaut renseignées) : { "user" : "root", "password" : "" }
+   */
+  static {
+    File commandsFile = new File("config/essentials-config/database.json");
+    if (commandsFile.exists()) {
+      try {
+        JsonObject database = new JsonParser().parse(new JsonReader(new FileReader(commandsFile))).getAsJsonObject();
+
+        String ip = database.get("ip").getAsString();
+        String user = database.get("user").getAsString();
+        String password = database.get("password").getAsString();
+
+        serverDatabase = database.get("serverDatabase").getAsString();
+        jdrDatabase = database.get("jdrDatabase").getAsString();
+        jdbcUrl = "jdbc:mysql://" + user + ":" + password + "@" + ip + "/";
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+  }
+
+  public static Connection getJdrConnection() {
     try {
       return sql.getDataSource(jdbcUrl + jdrDatabase).getConnection();
     } catch (SQLException e) {
@@ -34,39 +53,11 @@ public final class McFrConnection {
   }
 
   public static Connection getServerConnection() {
-    if (!configRead) {
-      readConfigFile();
-      configRead = true;
-    }
     try {
       return sql.getDataSource(jdbcUrl + serverDatabase).getConnection();
     } catch (SQLException e) {
       e.printStackTrace();
       return null;
-    }
-  }
-
-  /**
-   * Lit les identifiants de connexion renseignées dans le fichier de config : config/essentials-config/database.json.
-   * 
-   * Fichier de la forme (valeurs par défaut renseignées) : { "user" : "root", "password" : "" }
-   */
-  private static void readConfigFile() {
-    File commandsFile = new File("config/essentials-config/database.json");
-    if (commandsFile.exists()) {
-      try {
-        JsonObject database = new JsonParser().parse(new JsonReader(new FileReader(commandsFile))).getAsJsonObject();
-        
-        String ip = database.get("ip").getAsString();
-        String user = database.get("user").getAsString();
-        String password = database.get("password").getAsString();
-        
-        serverDatabase = database.get("serverDatabase").getAsString();
-        jdrDatabase = database.get("jdrDatabase").getAsString();
-        jdbcUrl = "jdbc:mysql://" + user + ":" + password + "@" + ip + "/";
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
     }
   }
 }
