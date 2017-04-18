69f29380cfc6f4581ba4a128e9d4229a387e02b0
Lepticed <cedtrabou@gmail.com>
2017-04-18 18:43:35 +0200

    refactor: Utilisation de stream.

--- essentials/src/main/java/net/mcfr/time/weather/BiomeGenres.java
+++ essentials/src/main/java/net/mcfr/time/weather/BiomeGenres.java
@@ -1,6 +1,8 @@
 package net.mcfr.time.weather;
 
-import java.util.ArrayList;
+import static org.spongepowered.api.world.biome.BiomeTypes.*;
+
+import java.util.Arrays;
 import java.util.List;
 
 import org.spongepowered.api.world.biome.BiomeType;
@@ -8,23 +10,23 @@ import org.spongepowered.api.world.biome.BiomeTypes;
 
 public enum BiomeGenres {
   // #f:0
-  PLAIN(0, 5, BiomeTypes.BEACH, BiomeTypes.FROZEN_RIVER, BiomeTypes.MUSHROOM_ISLAND, BiomeTypes.MUSHROOM_ISLAND_SHORE,
-      BiomeTypes.PLAINS, BiomeTypes.RIVER, BiomeTypes.SWAMPLAND, BiomeTypes.SWAMPLAND_MOUNTAINS,
-      BiomeTypes.SAVANNA, BiomeTypes.SAVANNA_MOUNTAINS, BiomeTypes.SAVANNA_PLATEAU, BiomeTypes.SAVANNA_PLATEAU_MOUNTAINS,
-      BiomeTypes.STONE_BEACH, BiomeTypes.SUNFLOWER_PLAINS),
-  FOREST(5, 5, BiomeTypes.BIRCH_FOREST, BiomeTypes.BIRCH_FOREST_HILLS, BiomeTypes.BIRCH_FOREST_HILLS_MOUNTAINS, BiomeTypes.BIRCH_FOREST_MOUNTAINS,
-      BiomeTypes.FLOWER_FOREST, BiomeTypes.FOREST, BiomeTypes.FOREST_HILLS, BiomeTypes.JUNGLE,
-      BiomeTypes.JUNGLE_EDGE, BiomeTypes.JUNGLE_EDGE_MOUNTAINS, BiomeTypes.JUNGLE_HILLS, BiomeTypes.JUNGLE_MOUNTAINS,
-      BiomeTypes.ROOFED_FOREST, BiomeTypes.ROOFED_FOREST_MOUNTAINS),
-  OCEAN(-3, 5, BiomeTypes.DEEP_OCEAN, BiomeTypes.FROZEN_OCEAN, BiomeTypes.SKY, BiomeTypes.VOID, BiomeTypes.HELL),
-  DESERT(20, 10, BiomeTypes.DESERT, BiomeTypes.DESERT_HILLS, BiomeTypes.DESERT_MOUNTAINS, BiomeTypes.MESA,
-      BiomeTypes.MESA_BRYCE, BiomeTypes.MESA_PLATEAU, BiomeTypes.MESA_PLATEAU_FOREST, BiomeTypes.MESA_PLATEAU_FOREST_MOUNTAINS,
-      BiomeTypes.MESA_PLATEAU_MOUNTAINS),
-  SNOWY(-7, 3, BiomeTypes.COLD_BEACH, BiomeTypes.COLD_TAIGA, BiomeTypes.COLD_TAIGA_HILLS, BiomeTypes.COLD_TAIGA_MOUNTAINS,
-      BiomeTypes.EXTREME_HILLS, BiomeTypes.EXTREME_HILLS_EDGE, BiomeTypes.EXTREME_HILLS_MOUNTAINS, BiomeTypes.EXTREME_HILLS_PLUS,
-      BiomeTypes.EXTREME_HILLS_PLUS_MOUNTAINS, BiomeTypes.ICE_MOUNTAINS, BiomeTypes.ICE_PLAINS, BiomeTypes.ICE_PLAINS_SPIKES,
-      BiomeTypes.MEGA_TAIGA, BiomeTypes.MEGA_TAIGA_HILLS, BiomeTypes.MEGA_SPRUCE_TAIGA, BiomeTypes.MEGA_SPRUCE_TAIGA_HILLS,
-      BiomeTypes.TAIGA, BiomeTypes.TAIGA_HILLS, BiomeTypes.TAIGA_MOUNTAINS);
+  PLAIN(0, 5, BEACH, FROZEN_RIVER, MUSHROOM_ISLAND, MUSHROOM_ISLAND_SHORE,
+      PLAINS, RIVER, SWAMPLAND, SWAMPLAND_MOUNTAINS,
+      SAVANNA, SAVANNA_MOUNTAINS, SAVANNA_PLATEAU, SAVANNA_PLATEAU_MOUNTAINS,
+      STONE_BEACH, SUNFLOWER_PLAINS),
+  FOREST(5, 5, BIRCH_FOREST, BIRCH_FOREST_HILLS, BIRCH_FOREST_HILLS_MOUNTAINS, BIRCH_FOREST_MOUNTAINS,
+      FLOWER_FOREST, BiomeTypes.FOREST, FOREST_HILLS, JUNGLE,
+      JUNGLE_EDGE, JUNGLE_EDGE_MOUNTAINS, JUNGLE_HILLS, JUNGLE_MOUNTAINS,
+      ROOFED_FOREST, ROOFED_FOREST_MOUNTAINS),
+  OCEAN(-3, 5, DEEP_OCEAN, FROZEN_OCEAN, SKY, VOID, HELL),
+  DESERT(20, 10, BiomeTypes.DESERT, DESERT_HILLS, DESERT_MOUNTAINS, MESA,
+      MESA_BRYCE, MESA_PLATEAU, MESA_PLATEAU_FOREST, MESA_PLATEAU_FOREST_MOUNTAINS,
+      MESA_PLATEAU_MOUNTAINS),
+  SNOWY(-7, 3, COLD_BEACH, COLD_TAIGA, COLD_TAIGA_HILLS, COLD_TAIGA_MOUNTAINS,
+      EXTREME_HILLS, EXTREME_HILLS_EDGE, EXTREME_HILLS_MOUNTAINS, EXTREME_HILLS_PLUS,
+      EXTREME_HILLS_PLUS_MOUNTAINS, ICE_MOUNTAINS, ICE_PLAINS, ICE_PLAINS_SPIKES,
+      MEGA_TAIGA, MEGA_TAIGA_HILLS, MEGA_SPRUCE_TAIGA, MEGA_SPRUCE_TAIGA_HILLS,
+      TAIGA, TAIGA_HILLS, TAIGA_MOUNTAINS);
   // #f:1
 
   private List<BiomeType> biomes;
@@ -34,24 +36,15 @@ public enum BiomeGenres {
   private BiomeGenres(int tempBonus, int tempVar, BiomeType... biomes) {
     this.temperatureBonus = tempBonus;
     this.temperatureVariation = tempVar;
-    
-    this.biomes = new ArrayList<>();
 
-    for (BiomeType biome : biomes) {
-      this.biomes.add(biome);
-    }
+    this.biomes = Arrays.asList(biomes);
   }
-  
+
   public int getTemperatureModificator(int hour) {
-    return this.temperatureBonus + (int) Math.floor(1f * this.temperatureVariation * Math.sin(2*Math.PI*(hour - 9)/24f));
+    return this.temperatureBonus + (int) Math.floor(1f * this.temperatureVariation * Math.sin(2 * Math.PI * (hour - 9) / 24f));
   }
-  
+
   public static BiomeGenres getGenreByBiome(BiomeType biome) {
-    for (BiomeGenres genre : BiomeGenres.values()) {
-      if (genre.biomes.contains(biome)) {
-        return genre;
-      }
-    }
-    return SNOWY;
+    return Arrays.stream(values()).filter(g -> g.biomes.contains(biome)).findFirst().orElse(SNOWY);
   }
 }
